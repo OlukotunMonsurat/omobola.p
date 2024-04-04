1b24bedcf9661140f0b696fbfd499deca4cedd48
OlukotunMonsurat <166024470+OlukotunMonsurat@users.noreply.github.com>
2024-04-04 15:33:28 +0100

    Add files via upload
    initial cmmit to git repository

---
+++ bola.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+    const body =
+    document.body;
+    body.classList.toggle('dark-mode');
+}
+const movingimage=
+document.getElementById('moving-image');
+
+/*document.addEventListener('mousemove',(e)=>{
+    const mouseX=e.clientX;
+    const mouseY= e.clientY;
+
+
+    movingimage.style.left=mouseX +'px';
+    movingimage.style.top=mouseY + 'px';
+});*/
